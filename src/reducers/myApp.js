e20e61675bdc3446bbd56e5a633b0019cbf59304
monasir <mohd.204@live.com>
2018-09-09 23:44:34 +0800

    without console.log

--- src/reducers/myApp.js
+++ src/reducers/myApp.js
@@ -1,19 +1,18 @@
 function Votes(state=[], action) {
     const newState = [...state];
     const i = action.index;
-    console.log(newState);
+    
     switch (action.type) {
       case 'VOTE_ANGULAR':
-        console.log("this is the i :"+i);
+        
         newState[i].angular++;
         return newState;
       case 'VOTE_REACT':
-        console.log("Your choice is React!")
-        console.log("this is the i :"+i);
+
         newState[i].react++;
         return newState;
       case 'VOTE_VUEJS':
-        console.log("Your choice is Vue.js")
+        
         newState[i].vuejs++;
         return newState;
       default:
